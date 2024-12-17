c1bb056f4469f13bd07cfc91d2ccb22a0cf88c1e
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2024-12-17 21:54:46 +0800

    Add CRUD endpoints for book creation, update, and deletion
    - Created POST endpoint to add a new book
    - Created PUT endpoint to update an existing book
    - Created DELETE endpoint to remove a book by its ID
    - Integrated BookService methods for handling book creation, update, and deletion

--- src/main/java/com/training/api/library/controller/LibraryController.java
+++ src/main/java/com/training/api/library/controller/LibraryController.java
@@ -3,8 +3,12 @@ package com.training.api.library.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +31,24 @@ public class LibraryController {
 	public Book getBookById(@PathVariable int bookId) {
 		return bookService.getBookById(bookId);
 	}
+	
+	@PostMapping("/books")
+	public Book createBook(@RequestBody Book book) {
+	    Book newBook = bookService.saveNewBook(book);
+	    return newBook;
+	}
+	
+	@PutMapping("/books")
+	public Book updateBook(@RequestBody Book book) {
+	    return bookService.updateBook(book);
+	}
+	
+	@DeleteMapping("/books/{bookId}")
+	public String removeBook(@PathVariable int bookId) {
+		Book book = bookService.getBookById(bookId);
+		bookService.deleteBook(book);
+		
+		return "Book with title " + book.getTitle() + " has been deleted";
+	}
+	
 }
