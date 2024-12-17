0903bf9fd7e02b72374e4fc04a1ece8a8bb625a1
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2024-12-17 21:17:04 +0800

    Add BookRepository with basic query methods
    - Added methods to find books by title and author
    - Added custom query to find books with a title containing a given string

---
+++ src/main/java/com/training/api/library/repository/BookRepository.java
@@ -0,0 +1,24 @@
+package com.training.api.library.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.training.api.library.entity.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+	
+	Optional<Book> findByTitle(String title);
+	
+	List<Book> findByAuthor(String author);
+	
+	// Using named parameter in the query
+    @Query("SELECT b FROM Book b WHERE b.title LIKE %:title%")
+    List<Book> findBooksByTitleContaining(@Param("title") String title);
+
+}
