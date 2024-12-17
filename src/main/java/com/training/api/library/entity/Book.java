ab22377fbebf55a3f3f16d3053e2456038e1b5e7
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2024-12-17 21:15:20 +0800

    Created Book Entity base on the database table books

---
+++ src/main/java/com/training/api/library/entity/Book.java
@@ -0,0 +1,91 @@
+package com.training.api.library.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@Column(name = "author")
+	private String author;
+	
+	@Column(name = "genre")
+	private String genre;
+	
+	@Column(name = "publication_year")
+	private int pubYear;
+	
+	@Column(name = "isbn")
+	private String isbn;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public int getPubYear() {
+		return pubYear;
+	}
+
+	public void setPubYear(int pubYear) {
+		this.pubYear = pubYear;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", genre=" + genre + ", pubYear="
+				+ pubYear + ", isbn=" + isbn + "]";
+	}
+	
+	
+	
+
+}
